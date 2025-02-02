24734806a720c262b03de9630ccef650c6575ab9
Italo Bruno Avelino <52352651+ItaloBrunoM@users.noreply.github.com>
2025-02-02 13:20:12 -0300

    tentativa 3

--- minCoin.c
+++ minCoin.c
@@ -4,21 +4,19 @@
 #define min(a, b) ((a) < (b) ? (a) : (b))
 
 int minimumCoins(int* prices, int pricesSize) {
-    int dp[pricesSize + 1];  
-     
+    int dp[pricesSize + 1];
+
     for (int i = 0; i <= pricesSize; i++) {
         dp[i] = INT_MAX;
     }
-    
     dp[0] = 0; 
 
-    for (int i = 0; i < pricesSize; i++) {
-        for (int j = i + 1; j <= pricesSize && j <= i + 1 + i; j++) {
-            dp[j] = min(dp[j], dp[i] + prices[i]);
+    for (int i = 1; i <= pricesSize; i++) {
+        for (int j = i - 1; j >= 0; j--) {
+            dp[i] = min(dp[i], dp[j] + prices[j]);
         }
     }
 
-    return dp[pricesSize];
+    return dp[pricesSize];  
 }
-
 
