dea3a073859af30deb6d89eeddf95933760f5076
Carlos Eduardo Mendes de Mesquita <58157127+CarlosEduardoMendesdeMesquita@users.noreply.github.com>
2025-02-06 17:47:30 -0300

    Erro
    Passa em um dos casos, onde não há compra, está faltando a parte de venda, tentarei implementar

--- bestTime.py
+++ bestTime.py
@@ -2,9 +2,6 @@ class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         lucro = 0
         for i in range(len(prices) - 1):
-            if(prices[i] > prices[i + 1]):
-               lucro += prices[i] - prices[i + 1]
-               return lucro
-            elif(prices[i+1] > prices[i]):
+            if(prices[i+1] > prices[i]):
                 lucro += prices[i+1] - prices[i]
             return lucro 
