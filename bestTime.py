4c1c37da0b9896d14cac4d3c20de5cf3778d19d4
Carlos Eduardo Mendes de Mesquita <58157127+CarlosEduardoMendesdeMesquita@users.noreply.github.com>
2025-02-06 17:50:56 -0300

    Acerto mudando return
    Erro estava em deixar o return na indentação do if, não havia percebido.

--- bestTime.py
+++ bestTime.py
@@ -4,4 +4,4 @@ class Solution:
         for i in range(len(prices) - 1):
             if(prices[i+1] > prices[i]):
                 lucro += prices[i+1] - prices[i]
-            return lucro 
+        return lucro 
