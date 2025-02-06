5f1146b81f20994879a46ba1df0a17b64dabc453
Carlos Eduardo Mendes de Mesquita <58157127+CarlosEduardoMendesdeMesquita@users.noreply.github.com>
2025-02-06 17:43:13 -0300

    Errado
    Primeiro envio deu errado, só compra uma vez no primeiro dia e não checa, vou tentar colocar return em ambas as clausulas do if

--- bestTime.py
+++ bestTime.py
@@ -1,3 +1,9 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        
+        lucro = 0
+        for i in range(len(prices) - 1):
+            if(prices[i] > prices[i + 1]):
+               lucro += prices[i] - prices[i + 1]
+            elif(prices[i+1] > prices[i]):
+                lucro += prices[i+1] - prices[i]
+            return lucro 
